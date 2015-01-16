c7ae59b3d1b7315b1db5117418188319e6000a1c
Hendy Irawan <ceefour666@gmail.com>
2015-01-16 07:26:56 -0430

    Server starter

---
+++ src/main/groovy/id/ac/itb/ee/lskk/lumen/persistence/LumenPersistenceApplication.groovy
@@ -0,0 +1,12 @@
+package id.ac.itb.ee.lskk.lumen.persistence
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class LumenPersistenceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run LumenPersistenceApplication, args
+    }
+}
---
+++ src/test/groovy/id/ac/itb/ee/lskk/lumen/persistence/LumenPersistenceApplicationTests.groovy
@@ -0,0 +1,16 @@
+package id.ac.itb.ee.lskk.lumen.persistence
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+@RunWith(SpringJUnit4ClassRunner)
+@SpringApplicationConfiguration(classes = LumenPersistenceApplication)
+class LumenPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
