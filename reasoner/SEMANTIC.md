cd62e7dff4f2b87f650b185949d20fda2357eb28
Hendy Irawan <ceefour666@gmail.com>
2015-10-28 12:49:41 +0700

    preferred synset mapping

---
+++ reasoner/SEMANTIC.md
@@ -0,0 +1,10 @@
+Reasoner menggunakan 4 file semantic:
+
+1. `src/main/resources/org/lskk/lumen/reasoner/nlp/preferred_physical_entity.csv`
+2. `src/main/resources/org/lskk/lumen/reasoner/nlp/preferred_scene.csv`
+3. `src/main/resources/org/lskk/lumen/reasoner/nlp/preferred_verb.csv`
+4. `src/main/resources/org/lskk/lumen/reasoner/nlp/preferred_adjective.csv`
+
+Semua file tersebut bersifat read-only, tidak diedit secara langsung.
+Untuk mengeditnya, edit file `Dropbox\Lumen\NLU\Lumen Natural Language Concepts.xlsx`
+lalu _Save As_ sheet yang relevan ke file CSV yang diinginkan. 
---
+++ reasoner/src/main/java/org/lskk/lumen/reasoner/nlp/PreferredMapper.java
@@ -0,0 +1,66 @@
+package org.lskk.lumen.reasoner.nlp;
+
+import com.google.common.collect.ImmutableMap;
+import com.opencsv.CSVReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Created by ceefour on 28/10/2015.
+ */
+@Service
+public class PreferredMapper {
+
+    public static final Locale INDONESIAN = Locale.forLanguageTag("id-ID");
+    private static final Logger log = LoggerFactory.getLogger(PreferredMapper.class);
+
+    private Map<String, Map<Locale, String>> preferredVerbs;
+    private Map<String, Map<Locale, String>> preferredPhysicalEntities;
+    private Map<String, Map<Locale, String>> preferredScenes;
+    private Map<String, Map<Locale, String>> preferredAdjectives;
+
+    public void init() throws IOException {
+        preferredPhysicalEntities = readPreferredCsv(PreferredMapper.class.getResource("preferred_physical_entity.csv"));
+        preferredScenes = readPreferredCsv(PreferredMapper.class.getResource("preferred_scene.csv"));
+        preferredVerbs = readPreferredCsv(PreferredMapper.class.getResource("preferred_verb.csv"));
+        preferredAdjectives = readPreferredCsv(PreferredMapper.class.getResource("preferred_adjective.csv"));
+    }
+
+    protected Map<String, Map<Locale, String>> readPreferredCsv(URL url) throws IOException {
+        final ImmutableMap.Builder<String, Map<Locale, String>> mab = ImmutableMap.builder();
+        try (final CSVReader reader = new CSVReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8), ';', '"', 1)) {
+            while (true) {
+                final String[] line = reader.readNext();
+                if (line == null) {
+                    break;
+                }
+                final String nodeName = line[0];
+                final String preferred_en_US = line[1];
+                final String preferred_id_ID = line[2];
+                final ImmutableMap<Locale, String> entry = ImmutableMap.of(Locale.US, preferred_en_US, INDONESIAN, preferred_id_ID);
+                mab.put(nodeName, entry);
+            }
+        }
+        final ImmutableMap<String, Map<Locale, String>> map = mab.build();
+        log.info("Loaded {} preferred synset mappings from {}", url);
+        return map;
+    }
+
+    public Optional<String> getPreferred(String nodeName, Locale locale) {
+        final Optional<String> physicalEntity = getPreferred(preferredPhysicalEntities, nodeName, locale);
+    }
+
+    public Optional<String> getPreferred(Map<String, Map<Locale, String>> map, String nodeName, Locale locale) {
+        return Optional.ofNullable(map.get(nodeName)).map(it -> it.get(locale));
+    }
+
+}
