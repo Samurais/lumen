42ae2c0f1df02b09ff8005976fa287a0264b318c
Hendy Irawan <ceefour666@gmail.com>
2015-10-29 20:30:52 +0700

    reasoner socmed twitter

--- reasoner/src/main/java/org/lskk/lumen/reasoner/socmed/ReasonerTwitterConfig.java
+++ reasoner/src/main/java/org/lskk/lumen/reasoner/socmed/ReasonerTwitterConfig.java
@@ -1,10 +1,21 @@
 package org.lskk.lumen.reasoner.socmed;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.lskk.lumen.socmed.AgentRepository;
+import org.lskk.lumen.socmed.TwitterApp;
+import org.lskk.lumen.socmed.TwitterAuthorization;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.PropertyConfiguration;
 
+import javax.inject.Inject;
+import java.io.File;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Created by ceefour on 29/10/2015.
@@ -12,8 +23,37 @@ import java.io.IOException;
 @Configuration
 public class ReasonerTwitterConfig {
 
+    private static final Logger log = LoggerFactory.getLogger(ReasonerTwitterConfig.class);
+    public static final String APP_ID = "lumen";
+    public static final String AGENT_ID = "arkan";
+
+    @Inject
+    private Environment env;
+    @Inject
+    private ObjectMapper mapper;
+
     @Bean
     public AgentRepository agentRepo() throws IOException {
         return new AgentRepository();
     }
+
+    @Bean
+    public TwitterApp twitterApp() throws IOException {
+        return mapper.readValue(new File("config/" + APP_ID + ".TwitterApp.jsonld"), TwitterApp.class);
+    }
+
+    @Bean
+    public TwitterAuthorization twitterAuthorization() throws IOException {
+        return mapper.readValue(new File("config/agent/" + AGENT_ID + ".TwitterAuthorization.jsonld"), TwitterAuthorization.class);
+    }
+
+    @Bean
+    public TwitterFactory twitterFactory() throws IOException {
+        final Properties twitterProps = new Properties();
+        twitterProps.put("oauth.consumerKey", twitterApp().getApiKey());
+        twitterProps.put("oauth.consumerSecret", twitterApp().getApiSecret());
+        final PropertyConfiguration twitterConf = new PropertyConfiguration(twitterProps);
+        return new TwitterFactory(twitterConf);
+    }
+
 }
