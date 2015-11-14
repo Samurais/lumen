1a76a8efc63a2d826e3bbdcc68a18c1f2c72ab2a
Hendy Irawan <ceefour666@gmail.com>
2015-11-14 22:20:00 +0700

    Drools Story still not working

--- reasoner/src/main/java/org/lskk/lumen/reasoner/goal/TellStory.java
+++ reasoner/src/main/java/org/lskk/lumen/reasoner/goal/TellStory.java
@@ -2,11 +2,14 @@ package org.lskk.lumen.reasoner.goal;
 
 import org.lskk.lumen.reasoner.story.Story;
 
+import java.util.UUID;
+
 /**
  * Goal that tells a story to a person.
  * Created by ceefour on 07/11/2015.
  */
 public class TellStory extends Goal {
+    private UUID id = UUID.randomUUID();
     private String storyId;
     private Story story;
     private Integer lastPropositionIndex;
@@ -56,5 +59,28 @@ public class TellStory extends Goal {
                 ", lastPropositionIndex=" + lastPropositionIndex +
                 ", done=" + done +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TellStory tellStory = (TellStory) o;
+
+        if (done != tellStory.done) return false;
+        if (!id.equals(tellStory.id)) return false;
+        if (storyId != null ? !storyId.equals(tellStory.storyId) : tellStory.storyId != null) return false;
+        return !(lastPropositionIndex != null ? !lastPropositionIndex.equals(tellStory.lastPropositionIndex) : tellStory.lastPropositionIndex != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + (storyId != null ? storyId.hashCode() : 0);
+        result = 31 * result + (lastPropositionIndex != null ? lastPropositionIndex.hashCode() : 0);
+        result = 31 * result + (done ? 1 : 0);
+        return result;
     }
 }
