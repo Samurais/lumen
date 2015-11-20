87df63c95b6c7c5ee9dcb7f0055d5f471d109c66
Hendy Irawan <ceefour666@gmail.com>
2015-11-20 14:44:56 +0700

    classpath fix

--- reasoner/reasoner.sh
+++ reasoner/reasoner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR="$(dirname $0)"
-java -Xms256m -Xmx256m -cp $SCRIPT_DIR'/target/dependency/*':$SCRIPT_DIR'/target/classes' org.lskk.lumen.reasoner.ReasonerApp "$@"
+java -Xms256m -Xmx256m -cp $SCRIPT_DIR'/target/classes':$SCRIPT_DIR'/target/dependency/*' org.lskk.lumen.reasoner.ReasonerApp "$@"
