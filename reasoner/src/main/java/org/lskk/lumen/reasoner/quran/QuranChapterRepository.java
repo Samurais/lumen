a7ca001eb2f9422ddce2670a3dc93e74d5e4cab9
Ainatul Radhiah <ainawind27@gmail.com>
2015-11-27 23:04:52 +0700

    belajar memasukkan quran sudah berhasil menampilkan secara dinamis, jadi ketika user ingin disebutkan ayat apa saja bisa keluar speech

--- reasoner/src/main/java/org/lskk/lumen/reasoner/quran/QuranChapterRepository.java
+++ reasoner/src/main/java/org/lskk/lumen/reasoner/quran/QuranChapterRepository.java
@@ -1,10 +1,14 @@
 package org.lskk.lumen.reasoner.quran;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by aina on 18/11/2015.
  */
 public interface QuranChapterRepository extends PagingAndSortingRepository<QuranChapter, String> {
-
+    @Query(value="SELECT id FROM sanad.quranchapter WHERE regexp_replace(lower(namewithtashkeel), '[^a-z]', '', 'g') = regexp_replace(lower(:q), '[^a-z]', '', 'g');",
+    nativeQuery = true)
+    String getIdByName(@Param("q") String q);
 }
