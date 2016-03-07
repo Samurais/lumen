6dedc421764fc98d8db7050e72f6900ce5ff82ea
Hendy Irawan <ceefour666@gmail.com>
2016-03-07 10:53:23 +0700

    Persistence launcher

---
+++ persistence/persistence.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+SCRIPT_DIR="$(dirname $0)"
+java -Xms256m -Xmx256m -Djava.awt.headless=true -cp $SCRIPT_DIR'/target/classes':$SCRIPT_DIR'/target/dependency/*' org.lskk.lumen.persistence.LumenPersistenceApplication "$@"
+
